35cd2c1a7c29cf878469d889bad7a656f2ced25b
Pablo18-web <72516173+Pablo18-web@users.noreply.github.com>
2020-11-30 18:34:59 +0100

    he añadido el movimiento al eje z que no lo tenia antes

--- Scenes/Benito Merchán Pablo/Assets/Scripts/MovimientodelaEsfera.cs
+++ Scenes/Benito Merchán Pablo/Assets/Scripts/MovimientodelaEsfera.cs
@@ -2,32 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovimientodelaEsfera : MonoBehaviour {
+public class MovimientodelaEsfera : MonoBehaviour
+{
 
 	public float speed;
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+
 		speed = 70f;
 
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		MoverEsfera();
 	}
 
-  void MoverEsfera() {
+	void MoverEsfera()
+	{
 		
-		float desplY = Input.GetAxis("Vertical");
-		transform.Translate(Vector3.up * Time.deltaTime * speed * desplY);
+		float desplZ = Input.GetAxis("Vertical");
+		transform.Translate(Vector3.right * Time.deltaTime * speed * desplZ);
+		
 		float desplX = Input.GetAxis("Horizontal");
 		transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
-		
-		
-		
+
+
+
 		float PosX = transform.position.x;
-		float PosY = transform.position.y;
+		float PosZ = transform.position.z;
 
 		if (PosX > -95 && PosX < 95)
 		{
@@ -40,21 +45,33 @@ public class MovimientodelaEsfera : MonoBehaviour {
 
 		}
 
-		else if (PosX > -3 && desplX < 0)
+		else if (PosX > 95 && desplX < 0)
 		{
 			transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
 		}
 
+		
+		if (PosZ > -95 && PosZ < 95)
+		{
+			transform.Translate(Vector3.forward * Time.deltaTime * speed * desplZ);
+		}
+
+		else if (PosZ < -95 && desplZ > 0)
+		{
+			transform.Translate(Vector3.forward * Time.deltaTime * speed * desplZ);
+
+		}
+
+		else if (PosZ > 95 && desplZ < 0)
+		{
+			transform.Translate(Vector3.forward * Time.deltaTime * speed * desplZ);
 
 
 
 
+
+		}
+		}
+		
 	}
 
-
-
-
-
-
-
-}
