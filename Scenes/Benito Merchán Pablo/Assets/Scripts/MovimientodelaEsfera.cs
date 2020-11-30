2f2de4717dac7a187591616fcd2cb4d730958adc
Pablo18-web <72516173+Pablo18-web@users.noreply.github.com>
2020-11-30 16:30:25 +0100

    movimiento de la esfera con joystick
    al llegar al limite no consigo que la pelota no pase lo que si que he notado es que cuando se sale de los limites del plano la pelota se realentiza

---
+++ Scenes/Benito Merchán Pablo/Assets/Scripts/MovimientodelaEsfera.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientodelaEsfera : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		
+		speed = 70f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoverEsfera();
+	}
+
+  void MoverEsfera() {
+		
+		float desplY = Input.GetAxis("Vertical");
+		transform.Translate(Vector3.up * Time.deltaTime * speed * desplY);
+		float desplX = Input.GetAxis("Horizontal");
+		transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+		
+		
+		
+		float PosX = transform.position.x;
+		float PosY = transform.position.y;
+
+		if (PosX > -95 && PosX < 95)
+		{
+			transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+		}
+
+		else if (PosX < -95 && desplX > 0)
+		{
+			transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+
+		}
+
+		else if (PosX > -3 && desplX < 0)
+		{
+			transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+		}
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+}
