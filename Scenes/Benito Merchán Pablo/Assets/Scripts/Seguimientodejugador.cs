2d49d803c4d2953b544c827e09f5f8017e2bfebd
Pablo18-web <72516173+Pablo18-web@users.noreply.github.com>
2020-11-30 17:19:13 +0100

    Seguimiento de la camara desde el eje Y y el Z

---
+++ Scenes/Benito Merchán Pablo/Assets/Scripts/Seguimientodejugador.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Seguimientodejugador : MonoBehaviour {
+	
+	[SerializeField] Transform playerPosition;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector3(playerPosition.position.x, playerPosition.position.y + 40, playerPosition.position.z - 10);
+	}
+}
