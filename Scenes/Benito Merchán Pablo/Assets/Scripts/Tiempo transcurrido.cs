889f2d55dd3a74e8455d59c8c02b690972de3681
Pablo18-web <72516173+Pablo18-web@users.noreply.github.com>
2020-11-30 19:14:22 +0100

    he añadido el prefab de la columna pero no me sale el codigo porque me estoy liando te pongo por escrito

---
+++ Scenes/Benito Merchán Pablo/Assets/Scripts/Tiempo transcurrido.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class texto : MonoBehaviour
+{
+    [SerializeField] string Tiempotranscurrido;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   // void Update() => Tiempotranscurrido.text = "TiempoTranscurrido"; //me da error con el . text y no entiendo muy bien el porque si es un serialize field de tipo string
+
+    
+    
+}
---
+++ Scenes/Benito Merchán Pablo/Assets/Scripts/crearcolumna.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crearcolumna : MonoBehaviour
+{
+    //te lo comento por el escrito porque me estoy desesperando y no lo veo y estoy muy agobiado a este codigo ni caso
+    
+    /*
+    [SerializeField] GameObject Columna;
+    [SerializeField] Transform RefPos;
+    [SerializeField] float distobstacle;
+    float posRamdom;
+    float 
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("ColumnCorrutine");
+        InicioColumnas();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            CrearColumna();
+        }
+    }
+    void CrearColumna();
+        float posRandom = Random.Range(0f, 30f);
+        Vector3 DestPos = new Vector3(posRandom, 0, posRamdom);
+        Vector3 NewPos = RefPos.position + DestPos;
+         Instantiate(MyColumn, NewPos, Quaternion.identity);
+
+    */
+
+
+
+
+
+
+}
